c67e2af51ccd4b9440d6d9dc7e7bbfe983b46fc3
WuliJy <55351635+WuliJy@users.noreply.github.com>
2022-09-09 20:45:04 +0800

    远程库修改代码

--- src/main/java/com/git_iead/GitTest.java
+++ src/main/java/com/git_iead/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Test3");
         System.out.println("Test branch 1");
+        System.out.println("Test 从远程库修改");
     }
 }
